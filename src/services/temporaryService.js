2eb693a6033c21988d23048f780434f4beb467f7
WadimSh <wadim11@yandex.ru>
2025-11-11 15:40:41 +0300

    fix

--- src/services/temporaryService.js
+++ src/services/temporaryService.js
@@ -2,7 +2,7 @@ const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://mp.sharik.ru';
 
 export const syncUserToBackend = async (userData) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/api/users/simple`, {
+    const response = await fetch(`${API_BASE_URL}/api/simple-user/create`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
